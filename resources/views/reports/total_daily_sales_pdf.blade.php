b4ec392855d5a9dae9a375ae14a20c26463946b7
Beatus K <beatuskibiti@gmail.com>
2020-03-05 17:17:57 +0300

    total daily sales

---
+++ resources/views/reports/total_daily_sales_pdf.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Total Daily Sales Reports</title>
+
+</head>
+
+<body>
+    <div class="row">
+        <div id="container">
+            <div class="logo-container">
+                {{-- <img src="{{public_path('fileStore/logo/'.$pharmacy['logo'])}}" /> --}}
+            </div>
+        </div>
+    </div>
+    <div class="row" style="padding-top: -2%">
+
+        <h4 align="center">Total Daily Sales Reports</h4>
+
+
+        <div class="row">
+
+            <table style="width:100%">
+
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Total Sales</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $d)
+                    <tr>
+                        <td>{{date_format(new DateTime($d->date),'d M Y')}}</td>
+                        <td>{{number_format($d->amount,2)}}</td>
+                    </tr>
+                    @endforeach
+
+
+                </tbody>
+
+            </table>
+        </div>
+   
+    </div>
+
+
+
+</body>
+
+</html>
